3ac115752811b95bf7bf24c3d281aa057889a0d6
Fzr-Cyber <csfzr.bib@gmail.com>
2026-06-21 02:31:18 +0700

    Update save-saldo-awal.js

--- api/save-saldo-awal.js
+++ api/save-saldo-awal.js
@@ -1,11 +1,18 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') return res.status(405).json({ error: 'Metode tidak diizinkan' });
     try {
-        // Ganti URL di bawah dengan URL Web App Google Sheets Anda nanti
+        const { saldo_idr, saldo_usd } = req.body;
+        const username = req.body.username || "MASTER ADMIN";
+        
+        // Ganti teks di bawah dengan tautan URL Web App Google Apps Script Anda yang baru saja didapat
         await fetch('https://script.google.com/macros/s/AKfycbxq4q3zqR09ePp_00SIb5cM9iJeC95NGVFL3u_c12g9nAQBuh-0DxZXc0Q7GW92gykz5Q/exec', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(req.body)
+            body: JSON.stringify({
+                action: "save_saldo_awal",
+                username: username,
+                saldo_idr: saldo_idr,
+                saldo_usd: saldo_usd
+            })
         });
         return res.status(200).json({ success: true });
     } catch (error) {
