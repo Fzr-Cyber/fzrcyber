dbb3f28b3a68edd86c998a0369f3f87787e9b114
Fzr-Cyber <csfzr.bib@gmail.com>
2026-06-21 02:30:33 +0700

    Update get-saldo-awal.js

--- api/get-saldo-awal.js
+++ api/get-saldo-awal.js
@@ -1,8 +1,10 @@
 export default async function handler(req, res) {
-    if (req.method !== 'GET') return res.status(405).json({ error: 'Metode tidak diizinkan' });
     try {
-        // Ganti URL di bawah dengan URL Web App Google Sheets Anda nanti
-        let response = await fetch('URL_WEB_APP_GOOGLE_SHEETS_ANDA');
+        // Ambil nama user aktif dari kiriman frontend web Anda
+        const username = req.query.username || "MASTER ADMIN";
+        
+        // Ganti teks di bawah dengan tautan URL Web App Google Apps Script Anda yang baru saja didapat
+        let response = await fetch(`https://script.google.com/macros/s/AKfycbxq4q3zqR09ePp_00SIb5cM9iJeC95NGVFL3u_c12g9nAQBuh-0DxZXc0Q7GW92gykz5Q/exec?req=get_saldo_awal&username=${encodeURIComponent(username)}`);
         let data = await response.json();
         return res.status(200).json(data);
     } catch (error) {
